7805e8b7c37e7b92b649c6953617b3198aac3c7b
Isaace19 <iabella.4576@gmail.com>
2024-11-25 09:46:56 -0500

    Adding in new scripts and configurations for characters

--- world/scripts/characters/Base_Character.gd
+++ world/scripts/characters/Base_Character.gd
@@ -6,4 +6,64 @@
 
 extends CharacterBody2D
 
+class_name BaseCharacter 
+var character_name: String = ""
+var stats: Dictionary = {}
 
+func _init(character_name: String, base_stats: Dictionary):
+		name = character_name
+		stats = base_stats.duplicate() 
+
+# Common Stats that every character should have
+
+func attack(target):
+		#implement attack logic onto enemy character
+		pass
+
+func interact():
+		#implement interaction logic on node objects
+		pass
+
+
+#virtual function to be overidden by child classes
+func special_ability():
+		push_error("special_ability() must be implemented in derived character classes")
+
+
+
+
+# Adding in movement scripts to the base character for better practice
+# set tile size in game, default to 16pixels
+# create a moving variable
+
+const tile_size = 16
+var moving = false
+
+var input_dir
+func _physics_process(delta: float) -> void:
+	input_dir = Vector2.ZERO
+	if Input.is_action_pressed("ui_down"):
+		input_dir = Vector2(0,1)
+		move()
+	elif Input.is_action_pressed("ui_up"):
+		input_dir = Vector2(0,-1)
+		move()
+	elif Input.is_action_pressed("ui_right"):
+		input_dir = Vector2(1,0)
+		move()
+	elif Input.is_action_pressed("ui_left"):
+		input_dir = Vector2(-1,0)
+		move()
+	move_and_slide()
+
+func move():
+	# verify move when action is pressed
+	if input_dir:
+		if moving == false:
+			moving = true
+			var tween = create_tween()
+			tween.tween_property(self, "position", position + input_dir*tile_size, 0.35)
+			tween.tween_callback(move_false) # moving is now false
+
+func move_false():
+	moving = false
---
+++ world/scripts/characters/Player_Character.gd
@@ -0,0 +1,51 @@
+###################################
+# Name: Isaac Abella
+# Description: Class PlayerCharacter inherits BaseCharacter, this script
+# will include player inventory, level up and experience system, and should give direct control to the user
+###################################
+
+extends "res://scripts/characters/Base_Character.gd"
+
+class_name Player_Character
+
+var inventory: Array = []
+
+# set a max inventory size for my sanity
+var max_inventory: int = 10
+
+# function to check if item is in inventory
+func has_item(item: String)-> bool:
+		return item in inventory
+
+# function to add items to inventory
+func add_item(item: String) -> bool:
+		if inventory.size() >= max_inventory:
+				print("Inventory is currently full")
+				return false
+		inventory.append(item)
+		print(item, "Added to inventory!")
+		return true
+
+# function to remove items from inventory
+func remove_item(item: String) -> bool:
+		if item in inventory:
+				iventory.erase(item)
+				print(item, "Removed from inventory!")
+				return true
+		print(item, "not found in inventory!")
+		return false
+
+# function to list all of the items in your inventory
+func show_inventory():
+		print("Inventory:", inventory)
+
+
+# function to use items such as potions, add to later...
+func use_item(item: String):
+		if not has_item(item):
+				print("You don't have a", item)
+				return
+		if item == "Potion":
+				stats["health" += 20
+				print("You used a Potion! Your health is now: ", stats["health"])
+				remove_item(item)
