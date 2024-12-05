2795cd27e39f600887d2364c8d1163e8921f67a3
Isaac <iabella.4576@gmail.com>
2024-12-05 16:21:39 -0500

    Adding in theoretical files from windows before closing this repo and moving to mac - Isaac A

---
+++ world/scripts/turn_manager/custom_sort.gd
@@ -0,0 +1,18 @@
+###################################
+# Name: Isaac Abella
+# Description: This script should be in charge of sorting every character with a dictionary into a vector
+# than the vector implements a queue like sorting function to determine what everyone's speed is to go first
+# this sorting function should be called inside of turnbase.gd
+###################################
+
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
